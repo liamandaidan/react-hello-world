b34d74dfddabd1127561b76661a94c3dea28d507
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-03-05 21:11:02 -0700

    Form data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import "./App.css";
-import Greet from "./components/Greet";
-// import Welcome from './components/Welcome'
-// import Hello from './components/Hello'
-// import Prop from './components/Prop';
-// import Message from './components/Message';
-// import Counter from './components/Counter'
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-import ParentComponet from "./components/ParentComponet";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
-import Index from "./components/Index";
-import StyleSheet from "./components/StyleSheet";
-import Inline from "./components/Inline";
 import "./appStyles.css";
+import NewForm from "./components/NewForm";
 
-import styles from "./appStyles.module.css";
-import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
-      {/* <Greet name="liam" heroName="ya boi" /> */}
-      {/* <FunctionClick />
-      <ClassClick /> */}
-      <Form></Form>
+      <NewForm />
     </div>
   );
 }
---
+++ src/components/NewForm.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+export class NewForm extends Component {
+  //step 2 create constructor to house var
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      comments: "",
+    };
+  }
+  //step3 create a class prop to handle change
+  handleUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  handleComment = (event) => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    alert(`${this.state.username} ${this.state.comments}`);
+  };
+
+  //step 1 create forms
+  //for submit btn we will have to link it to form onSubmit
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUsername}
+          ></input>
+        </div>
+        <div>
+          <label>Comments</label>
+          <textarea
+            value={this.state.comments}
+            onChange={this.handleComment}
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default NewForm;
